ee8e2efc00da841bfd88751b0c69a660d4857f52
AshwanthHariharan <72878178+AshwanthHariharan@users.noreply.github.com>
2021-06-02 21:03:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function setup() {
+
+canvas = createCanvas(300, 300);
+    
+canvas.center();
+
+video = createCapture(VIDEO);
+
+video.hide();
+    
+}
+
+function draw() {
+
+image(video,300,300,0,0);
+
+}
